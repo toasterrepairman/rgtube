55855d15faf64a99e26eefd89f8642f371a73cbe
toasterrepairman <smol@toast.cyou>
2021-06-05 16:59:50 -0400

    preparing for logging feature

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate gio;
 
 use gtk::prelude::*;
 use gio::prelude::*;
+use std::io::BufReader;
 
 fn main() {
 
@@ -19,8 +20,13 @@ fn main() {
     }
     let glade_src = include_str!("rgtube.glade");
     let builder = gtk::Builder::from_string(glade_src);
+    let print_operation = gtk::PrintOperation::new();
 
+    // initialize widgets
     let window: gtk::Window = builder.get_object("window1").unwrap();
+    let console: gtk::TextView = builder.get_object("console").unwrap();
+    let button: gtk::Button = builder.get_object("downloadbutton").unwrap();
+    let input: gtk::Entry = builder.get_object("link").unwrap();
 
 /*  window mapping
 
@@ -28,16 +34,23 @@ fn main() {
     let dialog: gtk::MessageDialog = builder.get_object("messagedialog1").unwrap();
 */
 
-/*  button mapping below
-    
     button.connect_clicked(move |_| {
-        dialog.run();
-        dialog.hide();
+        let link = input.get_text().to_string();
+        download_video(&link);
     });
-*/
+
 
     window.show_all();
 
     gtk::main();
 
+}
+
+async fn download_video(link: &str) -> String {
+    let download_path: String = rustube::download_best_quality(&link).await
+        .unwrap()
+        .as_path()
+        .display()
+        .to_string();
+    return download_path;
 }
